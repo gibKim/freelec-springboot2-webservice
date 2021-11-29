2c02ed412af64499799b9182048b4a9880628344
gibK <gbkim0909@gmail.com>
2021-11-30 00:57:15 +0900

    무중단 배포

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-function  find_idle_profile() {
+function find_idle_profile() {
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
   if [${RESPONSE_CODE} -ge 400 ]
--- src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
+++ src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
@@ -1,4 +1,4 @@
-package com.jojoldu.book.springboot.web.dto;
+package com.jojoldu.book.springboot.web;
 
 import java.util.Arrays;
 import java.util.List;
